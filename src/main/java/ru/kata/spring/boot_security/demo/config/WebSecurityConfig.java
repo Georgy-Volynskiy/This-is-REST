dd3ac7de34261e2e4b76ecbc8cce3340c414093b
Georgy-Volynskiy <126697927+Georgy-Volynskiy@users.noreply.github.com>
2023-05-19 12:12:51 +0300

    Задание выполнено

--- src/main/java/ru/kata/spring/boot_security/demo/config/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/config/WebSecurityConfig.java
@@ -1,49 +1,47 @@
-package ru.kata.spring.boot_security.demo.configs;
+package ru.kata.spring.boot_security.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final SuccessUserHandler successUserHandler;
 
-    public WebSecurityConfig(SuccessUserHandler successUserHandler) {
+    private final AuthProviderImpl authProvider;
+
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, @Lazy AuthProviderImpl authProvider) {
         this.successUserHandler = successUserHandler;
+        this.authProvider = authProvider;
+
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/", "/users", "/users/new").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                //!!!!!
                 .formLogin().successHandler(successUserHandler)
                 .permitAll()
                 .and()
-                .logout()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/users")
                 .permitAll();
+
     }
 
-    // аутентификация inMemory
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("user")
-                        .roles("USER")
-                        .build();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider);
 
-        return new InMemoryUserDetailsManager(user);
     }
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleRepository.java
@@ -0,0 +1,13 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.entity.Role;
+
+import java.util.List;
+
+public interface RoleRepository {
+
+    void saveRole(Role role);
+
+    List<Role> getAllRoles();
+
+}
